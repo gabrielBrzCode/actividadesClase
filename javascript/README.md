1b8710e2a4171161152b1e8673ebd0acc8f81b12
Gabrzcode <agugabriel94@gmail.com>
2024-06-04 22:03:06 -0500

    enclase

---
+++ javascript/README.md
@@ -0,0 +1,15 @@
+## FUNDAMENTOS DE JAVASCRIPT##
+
+## DESCRIPCION##
+
+#Los conceptos basicos que estaremos abordando durante las proximas clases#( concecptos basicos de programacion)
+
+1. variables y tipos de datos
+2. Funciones
+3. Control de flujo -> condicionales y los ciclos
+
+se enfoca en la interactividad del sitio web -> intercaccion del navegador
+
+4. manejo del DOM -> 
+5. manejo de eventos
+6. Js asincronico y funciones asincronicas -> promesas
---
+++ javascript/scripts/conceptosBasicos.js
@@ -0,0 +1,41 @@
+// 1. lo primero es verificar que la vinculacion es correcta
+// ENVIAMOS UN MENSAJITO
+
+console.log("HOLA! MUNDO");
+// 1. Variables en JS
+
+/* DEFINICION -> espacio de memoria -> cajita que me guarda informacion en forma de datos -> diferentes tipos, numericos (2, 2.3,4 etc) boleanos, arreglos y texto, objetos*/
+// Definimos para usarlo contantemente
+
+
+/*
+VARIABLES EN JS
+1. VAR > Nos guarda datos que pueden cambiar en el tiempo( en desuso)
+2. LET >Nos guarda datos que pueden cambiar en el tiempo
+3. CONST > nos guarda datos constantes NO CAMBIAN NUNCA
+*/
+
+
+//2. DECLARACION DE VARIABLES
+
+/* 1. definir que tipo de variable ( let o const)
+2. darle nombre a la variable
+3. decirle que va a gaurdar
+*/
+let nombreVariable = "nombre" ;
+// numericos
+let numero = 5;
+const pi =3.14; 
+// texto -> Strings
+
+let texto = "hola";
+const nombre = "gabriel";
+
+// boleanos -> verdadero o falso
+let esVerdadero = true;
+const esFalso = false;
+
+// arreglos guardar mas de un dato DEL MISMO TIPO
+// se reconocen por el uso de [] y los datos se separan por comas
+let arregloNumeros = [5, 6, 7 ,3];
+const arregloNombres = [luisa, daniel, gabriel];
